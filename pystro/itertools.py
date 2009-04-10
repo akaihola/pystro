42d57dd71f34801c3c6734822c966d095aef9309
Antti Kaihola <akaihol+pystro@ambitone.com>
2009-04-10 14:56:56 +0300

    Added the itertools.filtered_split function. Added build directory to .gitignore.

--- pystro/itertools.py
+++ pystro/itertools.py
@@ -11,3 +11,17 @@ def filtered_join(delimiter, *items, **kw):
     return delimiter.join(
         filter(kw.get('filt'),
                [item.strip() for item in items if item is not None]))
+
+def filtered_split(delimiter, s, **kw):
+    '''
+    Split string with delimiter but drop empty and blank parts.
+
+    >>> filtered_split(',', '')
+    []
+    >>> filtered_split(',', 'A  , , B,C,')
+    ['A', 'B', 'C']
+    >>> filtered_split(',', 'A,B,C', filt=lambda s: s!='B')
+    ['A', 'C']
+    '''
+    return filter(kw.get('filt'), [item.strip()
+                                   for item in s.split(delimiter)])
